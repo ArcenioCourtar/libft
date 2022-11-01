721c1b24c3835ecde621efe24e47f2fceaca7f14
Arcenio Courtar <71286201+ArcenioCourtar@users.noreply.github.com>
2022-11-01 09:17:21 +0100

    From convoluted mess to three(3!) while loops

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,72 +6,49 @@
 /*   By: acourtar <acourtar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/10 14:30:28 by acourtar          #+#    #+#             */
-/*   Updated: 2022/11/01 08:55:25 by acourtar         ###   ########.fr       */
+/*   Updated: 2022/11/01 09:16:13 by acourtar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	ft_isspace(const char *str, int i);
-static int	state_changer(const char *nptr, int i, int s, int *negativenum);
+static int	atoi_isspace(const char *str);
 
-// TODO: JUST USE THREE WHILE LOOPS LMAO
-// Return int equivalent of string 'nptr'.
-int	ft_atoi(const char *nptr)
+// Return int equivalent of string 'str'.
+int	ft_atoi(const char *str)
 {
-	unsigned int	i;
-	int				s;
-	int				negativenum;
-	int				number;
+	long	number;
+	int		is_negative;
 
-	i = 0;
 	number = 0;
-	s = 0;
-	negativenum = 0;
-	while (i < ft_strlen(nptr))
+	is_negative = 0;
+	while (atoi_isspace(str))
+		str++;
+	if (*str == '+' || *str == '-')
 	{
-		s = state_changer(nptr, i, s, &negativenum);
-		if (s == 2)
-		{
-			number *= 10;
-			number += nptr[i] - '0';
-		}
-		else if (s == 3)
-			break ;
-		i++;
+		if (*str == '-')
+			is_negative = 1;
+		str++;
 	}
-	if (negativenum == 1)
+	while (*str >= '0' && *str <= '9')
+	{
+		number *= 10;
+		number += *str - '0';
+		str++;
+	}
+	if (is_negative == 1)
 		number *= -1;
-	return (number);
+	return ((int) number);
 }
 
 // Similar to the other 'isx' functions, but checks for spaces and
 // space-like characters.
 // Why isn't this part of libft?????? cringe.
-static int	ft_isspace(const char *str, int i)
+static int	atoi_isspace(const char *str)
 {
-	if (str[i] == ' ' || str[i] == '\n' || str[i] == '\f')
+	if (*str == ' ' || *str == '\n' || *str == '\f')
 		return (1);
-	if (str[i] == '\r' || str[i] == '\t' || str[i] == '\v')
+	if (*str == '\r' || *str == '\t' || *str == '\v')
 		return (1);
 	return (0);
-}
-
-// Tracks what characters have been seen earlier so it can
-// start or stop tracking actual numbers.
-static int	state_changer(const char *nptr, int i, int s, int *negativenum)
-{
-	if (ft_isspace(nptr, i) && s == 0)
-		return (0);
-	else if (nptr[i] == '+' && s == 0)
-		return (1);
-	else if (nptr[i] == '-' && s == 0)
-	{
-		*negativenum = 1;
-		return (1);
-	}
-	else if (nptr[i] >= '0' && nptr[i] <= '9' && (s == 1 || s == 0 || s == 2))
-		return (2);
-	else
-		return (3);
 }
